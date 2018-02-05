e8ee1a4c33b8dbdfde0631ee339075beca4c73dd
BIT Student <classroom@bgit.rs>
2018-02-05 11:16:27 +0100

    switch2-vezba

---
+++ switch/switch2.js
@@ -0,0 +1,29 @@
+var day = 31;
+
+switch (day) {
+    case 1:
+        console.log("monday");
+        break;
+
+    case 2:
+        console.log("thusday");
+        break;
+    case 3:
+        console.log("wednsday");
+        break;
+    case 4:
+        console.log("thursday");
+        break;
+    case 5:
+        console.log("fraiday");
+        break;
+    case 6:
+        console.log("suturday");
+        break;
+    case 7:
+        console.log("sunday");
+        break;
+    default:
+        console.log("Input must be a number between 1 and 7");
+        break;
+}
