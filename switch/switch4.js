8e3c665798fa1c76846d793b59ddf3e887ac6675
BIT Student <classroom@bgit.rs>
2018-02-05 11:32:50 +0100

    switch4-vezba

---
+++ switch/switch4.js
@@ -0,0 +1,44 @@
+var month = 1;
+
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+
+    case 2:
+        console.log("February");
+        break;
+    case 3:
+        console.log("March");
+        break;
+    case 4:
+        console.log("April");
+        break;
+    case 5:
+        console.log("May");
+        break;
+    case 6:
+        console.log("June");
+        break;
+    case 7:
+        console.log("July");
+        break;
+    case 9:
+        console.log("August");
+        break;
+    case 9:
+        console.log("September");
+        break;
+    case 10:
+        console.log("October");
+        break;
+    case 11:
+        console.log("November");
+        break;
+    case 12:
+        console.log("December");
+        break;
+    default:
+        console.log("Input must be a number between 1 and 12");
+        break;
+}
