c05bf84f2e97a99c7970051ec3b2744eb6121b1b
BIT Student <classroom@bgit.rs>
2018-02-27 18:02:59 +0100

    closures

---
+++ objects/Closures_bind_call_apply/closures.js
@@ -0,0 +1,18 @@
+function EmpMan(name, surname, specialization, salary) {
+    this.name = name;
+    this.surname = surname;
+    this.specialization = specialization;
+    this.salary = salary;
+    
+    var getSpecialization = function() {
+        return this.specialization
+    }
+    function Managers() {
+        var getDepartment = function() {
+            name.specialization
+        }
+    var changeDepartment = function() {
+
+    }
+    }
+}
