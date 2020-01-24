8d6cc9869ac1f9aeb4444536df992f470fe5edca
Želimir Ilić <zelimir.ilic@consolia.rs>
2020-01-24 16:39:41 +0100

    LudiCalculator

---
+++ JS-Udemy/basics-01-starting-project/assets/scripts/app.js
@@ -0,0 +1,80 @@
+const defaultResult = 0;
+let currentResult = defaultResult;
+let logEntries = [];
+
+
+// This function extracts the user input from input field
+function getUserNumberInput() {
+  return parseInt(userInput.value);
+}
+
+//Generates and writes calculation log
+function createAndWriteOutput(operator, resultBeforeCalc, calcNumber) {
+  const calcDescription = `${resultBeforeCalc} ${operator}${calcNumber}`;
+  outputResult(currentResult, calcDescription); // vendor.js
+}
+
+function add() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult += enteredNumber;
+  createAndWriteOutput('+', initialResult, enteredNumber );
+  const logEntry = {
+    operation: 'ADD',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function substract() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult -= enteredNumber;
+  createAndWriteOutput('-', initialResult, enteredNumber );
+  const logEntry = {
+    operation: 'SUB',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function multiply() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult *= enteredNumber;
+  createAndWriteOutput('*', initialResult, enteredNumber );
+  const logEntry = {
+    operation: 'MLPY',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+function divide() {
+  const enteredNumber = getUserNumberInput();
+  const initialResult = currentResult;
+  currentResult /= enteredNumber;
+  createAndWriteOutput('/', initialResult, enteredNumber );
+  const logEntry = {
+    operation: 'DIV',
+    prevResult: initialResult,
+    number: enteredNumber,
+    result: currentResult
+  };
+  logEntries.push(logEntry);
+  console.log(logEntries);
+}
+
+addBtn.addEventListener("click", add);
+subtractBtn.addEventListener("click", substract);
+multiplyBtn.addEventListener("click", multiply);
+divideBtn.addEventListener("click", divide);
---
+++ JS-Udemy/basics-01-starting-project/assets/scripts/proba.js
@@ -0,0 +1,8 @@
+let userName = 'Max';
+userName = 'Manu';
+greetUser('Pavle');
+
+function greetUser(name) {
+  let userName = name;
+  console.log(userName);
+}
